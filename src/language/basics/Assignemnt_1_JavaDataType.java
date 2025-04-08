f50eada42a53c12bec24d7d43d1485c58e09aeaf
pratikshagaikwad5150 <pratiksha.gaikwad5150@gmail.com>
2025-04-09 00:04:25 +0530

    Assignment

---
+++ src/language/basics/Assignemnt_1_JavaDataType.java
@@ -0,0 +1,96 @@
+package language.basics;
+
+public class Assignemnt_1_JavaDataType {
+
+	public static void main(String[] args) {
+		
+//      1. Temperature of a city in degrees Celsius: 25.5 (Data type:Float)
+		float temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius:" +temperature);
+		
+//		2. Whether a customer has placed an order: true or false(Data type:boolean))
+		boolean order1 = true;
+		boolean order2 = false;
+		System.out.println("Whether a customer has placed an order:" +order1);
+		System.out.println("Whether a customer has placed an order:" +order2);
+
+		//3. Person's phone number: "123-456-7890"(Data type:string)
+		String phonenumber = "123-456-7890";
+		System.out.println("Person's phone number: " +phonenumber);
+		
+//		4. Amount of money in a customer's bank account: 1000.50(Data type:Float)
+		float amount = 1000.50f;
+		System.out.println("Temperature of a city in degrees Celsius:" +amount);
+
+		//		5. Person's email address: "john.doe@example.com"(Data type:string)
+		String email_address ="john.doe@example.com";
+		System.out.println("Person's email address: " +email_address);
+		
+//		6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194(Data type:double)
+		double[] location1 = {37.7749,-122.4194};
+		System.out.println("Coordinates of a location (latitude, longitude):" +location1);
+
+		
+//      7. Person's marital status: true or false(Data type:boolean)
+		
+		boolean marital1 = true;
+		boolean marital21 = false;
+		System.out.println("Person's marital status:" +marital1);
+		System.out.println("Person's marital status:" +marital21);
+
+//		8. Person's occupation: "Software Engineer"(Data type:string)
+		String occupation =  "Software Engineer";
+		System.out.println("Person's occupation: " +occupation);
+
+//		9. Person's favourite colour: "Blue"(Data type:string)
+		String colour ="Blue";
+		System.out.println("Person's favourite colour: " +colour);
+
+//		10.Current year: 2023(Data type:short)
+		short year=2023;
+		System.out.println("Current year: " +year);
+		
+//		11.Number of followers on a social media platform: 1,000,000(Data type:string)
+		String followers ="1,00,0000";
+		System.out.println("Number of followers on a social media platform: " +followers);
+		
+//		12.Rating of a movie: 7.5(Data type:Float)
+		float rating = 7.5f;
+		System.out.println("Rating of a movie:" +rating);
+
+		
+//		13.Person's blood type: 'A'(Data type:char)
+		char bloodtype = 'A';
+		System.out.println("Rating of a movie:" +bloodtype);
+
+//		14.Title of a book: "To Kill a Mockingbird"(Data type:string)
+		String Title ="To Kill a Mockingbird";
+		System.out.println("Title of a book: " +Title);
+		
+//		15.Number of employees in a company: 500(Data type:short)
+		short noOfEmployee=500;
+		System.out.println("Number of employees in a company: " +noOfEmployee);
+		
+//		16.Time of an event: 2:30 PM(Data type:string)
+		String event =" 2:30 PM";
+		System.out.println("Time of an event: " +event);
+		
+//		17.Name of a country: "United States"(Data type:string)
+		String country ="United States";
+		System.out.println("Name of a country: " +country);
+		
+//		18.Person's eye color: "Brown"(Data type:string)
+		String eyeColor = "Brown";
+		System.out.println("Rating of a movie:" +eyeColor);
+		
+//		19.Person's birthplace: "New York City"(Data type:string)
+		String birthplace = "New York City";
+		System.out.println("Person's birthplace:" +birthplace);
+		
+//		20.Distance between two cities: 200.5(Data type:Float)
+		float Distance = 200.5f;
+		System.out.println("Distance between two cities:" +Distance);
+
+	}
+
+}
--- src/language/basics/DebuggingTheProgram.java
+++ src/language/basics/DebuggingTheProgram.java
@@ -3,12 +3,12 @@ package language.basics;
 public class DebuggingTheProgram {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("step 1");
 		System.out.println("step 2");
 		System.out.println("step 3");
 		System.out.println("step 4");
-		System.out.println("step 5"+5/(4-7+3));
+		System.out.println("step 5" + 5 / (4 - 7 + 3));
 
 	}
 
